7ce6eb395b933619c84411bf419e93c8187d66e9
zaky yusuf <59588009+zakyyusuff@users.noreply.github.com>
2022-11-02 18:34:38 +0700

    Add files via upload

---
+++ fungsi/functions.py
@@ -0,0 +1,35 @@
+# # def kalimat():
+# # import contoh2
+#     # kalimat_spam = []
+
+# if kalimat == ("kehabisan pulsa silahkan beli di konter kami"):
+#     print('promosi')
+# # elif kalimat == ("selamat pagi bapak dosen yang baik hati"):
+# #     print('normal')
+# # elif kalimat == ("ayo silahkan bermain di link kami dijamin menang"):
+# #     print('judi')
+# elif kalimat == ("silahkan transfer ke rekening saya"):
+#     print('penipuan')
+# else:
+#     print('coba cek kalimat anda mungkin typo')
+
+# --------------------------------------------------------------------
+# data = "haloo ini zaky"
+
+# def fungsi():
+#     print("ini adalah function zaky")
+
+# print("ini function zaky")
+
+# --------------------------------------------------------------------------
+# def fungsi():
+#     print("ini adalah function zaky")
+
+# data = "haloo ini zaky"
+
+# ------------------------------------------------------------------------
+
+data = "halo nama saya zaky muhammad yusuf"
+
+def cek_data():
+    print("data dari functions")
---
+++ fungsi/modul.py
@@ -0,0 +1,28 @@
+# print("isi data dari file import")
+
+# data = "halo nama saya zaky"
+# data = "halo nama saya zaky"
+
+# def cek_data():
+#     print("data dari modul")
+
+# def kondisi():
+#     nilai = 100
+
+#     if nilai is 100:
+#         print("nilai benar", nilai)
+# ----------------------------------------------------------
+
+# data = "halo nama saya zaky"
+
+# def cek_data():
+#     print("data dari modul")
+
+# ---------------------------------------------------------
+
+import functions
+
+print("isi data dari file import")
+
+functions.cek_data
+functions.data
