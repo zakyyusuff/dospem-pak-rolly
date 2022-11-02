73559068d81134d893c117ec1f332a6effe1aad9
zaky yusuf <59588009+zakyyusuff@users.noreply.github.com>
2022-11-02 18:36:33 +0700

    Add files via upload

---
+++ fungsi/functions.py
@@ -0,0 +1,4 @@
+data = "halo nama saya zaky muhammad yusuf"
+
+def cek_data():
+    print("data dari functions")
---
+++ fungsi/modul.py
@@ -0,0 +1,6 @@
+import functions
+
+print("isi data dari file import")
+
+functions.cek_data
+functions.data
