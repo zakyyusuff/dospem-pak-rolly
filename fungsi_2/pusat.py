0f526a0e18bb2418b3c22017ece12bc9a93eea18
zaky yusuf <59588009+zakyyusuff@users.noreply.github.com>
2022-11-07 21:03:32 +0700

    Add files via upload

---
+++ fungsi_2/pusat.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+dpus = "1194069 Zaky Muhammad Yusuf"
+
+def proses_data(data):
+    print("data sedang di proses...")
+    ubah_data = data + " telah dimodifikasi"
+    sleep(3)
+    print("proses ubah Data selesai.")
+    return ubah_data
